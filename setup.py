7235be010c29f9aaba81471afdacc35f4a068511
Dave Arter <github@davea.me>
2021-01-19 17:28:13 +0000

    v0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name = "doxieapi",
-    version = "0.0.3",
+    version = "0.1.1",
     packages = find_packages(),
 
     install_requires = ['requests'],
